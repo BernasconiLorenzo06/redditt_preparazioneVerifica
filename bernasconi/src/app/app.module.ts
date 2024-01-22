54d98ef4e4f1ef1d590388ea8520b12e7505ff36
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2024-01-22 11:38:07 +0000

    punto 7

--- bernasconi/src/app/app.module.ts
+++ bernasconi/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NuovoComponent } from './nuovo/nuovo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NuovoComponent } from './nuovo/nuovo.component';
     NuovoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
